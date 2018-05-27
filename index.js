4703284aa595ba3c37ea5a91c30965ce43b014a3
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:31 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
-import mergeRight from "@unction/mergeright"
-import reduceValues from "@unction/reducevalues"
-import fresh from "@unction/fresh"
-import first from "@unction/first"
-import of from "@unction/of"
-
-export default function indexBy (unction: RecordType => KeyType): UnaryFunctionType {
-  return function indexByUnction (list: ListType<RecordType>): RecordType<RecordType> {
-    return reduceValues(
-      (accumulated: RecordType<RecordType>): UnaryFunctionType =>
-        (value: ValueType): RecordType<RecordType> =>
-          mergeRight(
-            accumulated
-          )(
-            of(
-              unction(value)
-            )(
-              value
-            )(
-              accumulated
-            )
-          )
-    )(
-      fresh(first(Array.from(list)))
-    )(
-      list
-    )
-  }
+import mergeRight from "@unction/mergeright";
+import reduceValues from "@unction/reducevalues";
+import fresh from "@unction/fresh";
+import first from "@unction/first";
+import of from "@unction/of";
+export default function indexBy(unction) {
+  return function indexByUnction(list) {
+    return reduceValues(accumulated => value => mergeRight(accumulated)(of(unction(value))(value)(accumulated)))(fresh(first(Array.from(list))))(list);
+  };
 }
